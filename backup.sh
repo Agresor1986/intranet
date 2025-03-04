bd37f09b09185b9a93428b0682d9924fd1e5e5fe
Michael_Stredak <stredak.michael@gmail.com>
2025-03-04 14:13:43 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,6 +8,12 @@ DB_HOST="dpg-cv1glnl2ng1s738d0h4g-a.frankfurt-postgres.render.com"
 BACKUP_DIR="/tmp"
 FILENAME="backup_$(date +\%Y-\%m-\%d_\%H-\%M-\%S).sql"
 
+# Kontrola, či sú všetky premenné nastavené
+if [ -z "$DB_NAME" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ] || [ -z "$DB_HOST" ]; then
+  echo "Chýbajúce premenné! Skontrolujte GitHub Secrets."
+  exit 1
+fi
+
 # Inštalácia Rclone, ak chýba
 if ! command -v rclone &> /dev/null
 then
@@ -24,9 +30,16 @@ type = mega
 user = $MEGA_USER
 pass = $MEGA_PASS" > ~/.config/rclone/rclone.conf
 
+# Kontrola pripojenia k databáze
+echo "Kontrola pripojenia k databáze..."
+export PGPASSWORD="$DB_PASSWORD"
+if ! psql "postgresql://$DB_USER@$DB_HOST/$DB_NAME" -c "\q"; then
+  echo "Chyba pri pripájaní k databáze!"
+  exit 1
+fi
+
 # Export databázy
 echo "Export databázy..."
-export PGPASSWORD="$DB_PASSWORD"  # Nastavenie hesla pre pg_dump
 pg_dump -h $DB_HOST -U $DB_USER -d $DB_NAME > $BACKUP_DIR/$FILENAME
 
 # Kontrola, či sa export podaril
