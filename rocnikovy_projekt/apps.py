aa0e841ef2d8e140058524ef1fd6a8cc0ad52b3f
Michael_Stredak <stredak.michael@gmail.com>
2025-06-12 10:41:50 +0200

    Update apps.py

--- rocnikovy_projekt/apps.py
+++ rocnikovy_projekt/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class MyProjectConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "rocnikovy_projekt"  # Názov tvojho hlavného projektu
+    name = "rocnikovy_projekt" 
 
     def ready(self):
-        import rocnikovy_projekt.signals  # Načíta signály pri štarte projektu
+        import rocnikovy_projekt.signals  
