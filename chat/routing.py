1a5519e9d206636c3607a19f11042fd3a4761581
Michael_Stredak <stredak.michael@gmail.com>
2025-06-12 10:27:36 +0200

    Update routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -2,7 +2,7 @@ from django.urls import path
 from chat.consumers import ChatConsumer, PrivateChatConsumer
 
 websocket_urlpatterns = [
-    path("ws/group/", ChatConsumer.as_asgi()),  # pre skupinový chat
-    path("ws/private/<str:username>/", PrivateChatConsumer.as_asgi()),  # pre súkromný chat
+    path("ws/group/", ChatConsumer.as_asgi()),  
+    path("ws/private/<str:username>/", PrivateChatConsumer.as_asgi()),  
 ]
 
