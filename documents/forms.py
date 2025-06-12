37913b8886a6bb0a744f90a8d5edf1470c7cc7db
Michael_Stredak <stredak.michael@gmail.com>
2025-06-12 10:32:34 +0200

    Update forms.py

--- documents/forms.py
+++ documents/forms.py
@@ -1,14 +1,14 @@
-from django import forms  # Importuje modul na vytváranie formulárov v Django
-from .models import Document  # Importuje model Document
-# Trieda formulára pre nahrávanie dokumentov
+from django import forms  
+from .models import Document  
+
 class DocumentForm(forms.ModelForm):
     class Meta:
-        model = Document  # Určuje, že formulár bude založený na modeli Document
-        fields = ('title', 'file',)  # Definuje polia, ktoré sa zobrazia vo formulári
-    # Prispôsobenie formulára – pridanie popisov k poliam
+        model = Document  
+        fields = ('title', 'file',)  
+    
     def __init__(self, *args, **kwargs):
-        super(DocumentForm, self).__init__(*args, **kwargs)  # Zavolá pôvodnú inicializáciu formulára
-        self.fields['title'].label = 'Názov'  # Nastaví popis pre pole "title"
-        self.fields['file'].label = 'Súbor'  # Nastaví popis pre pole "file"
+        super(DocumentForm, self).__init__(*args, **kwargs)  
+        self.fields['title'].label = 'Názov'  
+        self.fields['file'].label = 'Súbor'  
 
 
